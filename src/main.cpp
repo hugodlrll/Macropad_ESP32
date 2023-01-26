4c69206676afa81dcece9793471a0d1e3dc85775
hugodlrll <hugo.del49@gmail.com>
2023-01-26 18:18:53 +0100

    ws avec deserialisation fonctionnel

--- src/main.cpp
+++ src/main.cpp
@@ -25,36 +25,46 @@
 #include <WiFi.h>
 #include <blekeyboard.h>
 #include <ArduinoWebsockets.h>
-
-BleKeyboard bleKeyboard;
-int button = 27;
+#include <ArduinoJson.h>
 
 const char* ssid = "wifirobot"; //Enter SSID
-const char* password = "16121998"; //Enter Password
-const char* websockets_server_host = "192.168.1.37"; //Enter server adress
+const char* password = "5cjWSgq7sefAnnJq"; //Enter Password
+const char* websockets_server_host = "192.168.10.58"; //Enter server adress
 const uint16_t websockets_server_port = 8050; // Enter server port
 
 using namespace websockets;
 
-void onMessageCallback(WebsocketsMessage message) {
-    Serial.print("Got Message: ");
-    Serial.println(message.data());
-}
-
-void onEventsCallback(WebsocketsEvent event, String data) {
-    if(event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connnection Opened");
-    } else if(event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connnection Closed");
-    } else if(event == WebsocketsEvent::GotPing) {
-        Serial.println("Got a Ping!");
-    } else if(event == WebsocketsEvent::GotPong) {
-        Serial.println("Got a Pong!");
-    }
-}
-
 WebsocketsClient client;
-void setup() {
+
+void ConfigReveived(WebsocketsMessage message) 
+{
+  
+  String data = message.data(); 
+  //data.replace("\"", "\"");
+  // Serial.println(data);
+  StaticJsonDocument<1024> doc;
+  DeserializationError error = deserializeJson(doc, data);
+  if (error) 
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  JsonObject TOUCHE1 = doc["action"];
+  //if(!TOUCHE1.isNull()){
+    const char* TOUCHE1_action = doc["action"];
+    const char* TOUCHE1_key1 = doc["key1"];
+    const char* TOUCHE1_key2 = doc["key2"]; 
+    const char* TOUCHE1_key3 = doc["key3"];
+    Serial.println(TOUCHE1_action);
+    Serial.println(TOUCHE1_key1);
+    Serial.println(TOUCHE1_key2);
+    Serial.println(TOUCHE1_key3);
+  //}
+}
+
+void setup() 
+{
     Serial.begin(115200);
     // Connect to wifi
     WiFi.begin(ssid, password);
@@ -65,37 +75,31 @@ void setup() {
         delay(1000);
     }
 
-    // run callback when messages are received
-    client.onMessage(onMessageCallback);
+    // Check if connected to wifi
+    if(WiFi.status() != WL_CONNECTED) {
+        Serial.println("No Wifi!");
+        return;
+    }
+
+    Serial.println("Connected to Wifi, Connecting to server.");
+    // try to connect to Websockets server
+    bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
+    if(connected) {
+        Serial.println("Connected!");
+        client.send("Hello Server");
+    } else {
+        Serial.println("Not Connected!");
+    }
     
-    // run callback when events are occuring
-    client.onEvent(onEventsCallback);
-
-    // Connect to server
-    client.connect(websockets_server_host, websockets_server_port, "/");
-
-    // Send a message
-    client.send("Hello Server");
-
-    // Send a ping
-    client.ping();
-
-    Serial.begin(115200);
-    pinMode(button, INPUT_PULLUP);
-    bleKeyboard.begin();
+    // run callback when messages are received
+    client.onMessage([&](WebsocketsMessage message)
+    {
+      //Serial.println("Got Message : " + message.data());
+      ConfigReveived(message);
+    } );
 }
 
-void loop() {
+void loop() 
+{
   client.poll();
-  if (digitalRead(button) == LOW) 
-  {
-    Serial.println("Button is pressed");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
-    delay(20);
-    bleKeyboard.releaseAll();
-    while(digitalRead(button) == LOW);
-  }
-
 }
