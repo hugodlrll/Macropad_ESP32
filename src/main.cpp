6bb40d517de6865138dbc0952ea5fa49eb3fde54
hugodlrll <hugo.del49@gmail.com>
2023-01-28 19:24:24 +0100

    modif ws

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,3 @@
-// #include <BleKeyboard.h>
-// #include <Arduino.h>
-
-// BleKeyboard bleKeyboard;
-
-// int button = 13;
-
-// void setup() {
-//   Serial.begin(115200);
-//   pinMode(button, INPUT_PULLUP);
-//   bleKeyboard.begin();
-// }
-
-// void loop() {
-//   if (digitalRead(button) == LOW) 
-//   {
-//     Serial.println("Button is pressed");
-//     bleKeyboard.press(KEY_LEFT_CTRL);
-//     bleKeyboard.press(KEY_LEFT_ALT);
-//     bleKeyboard.press(KEY_DELETE);
-//     bleKeyboard.releaseAll();
-//     while(digitalRead(button) == LOW);
-//   }
-//   delay(20);
-// }
-
 #include <WiFi.h>
 #include <blekeyboard.h>
 #include <ArduinoWebsockets.h>
@@ -34,13 +8,15 @@
 TaskHandle_t task1;
 TaskHandle_t task2;
 const char* action;
+String isMedia;
 char key1;
 char key2; 
 char key3;
+uint8_t media[2] = {};
 
-const char * ssid = "wifirobot"; //"SFR_43A0";   //Enter SSID
-const char * password = "5cjWSgq7sefAnnJq"; //"16121998";   //Enter Password
-const char * websockets_server_host = "192.168.10.58"; //Enter server adress
+const char * ssid = "SFR_1E88"; //"SFR_43A0";   //Enter SSID
+const char * password = "16121998"; //"16121998";   //Enter Password
+const char * websockets_server_host = "192.168.1.39"; //Enter server adress
 const uint16_t websockets_server_port = 8050; // Enter server port
 
 using namespace websockets;
@@ -52,7 +28,7 @@ int button = 13;
 void ConfigReveived(WebsocketsMessage message) 
 {
   String data = message.data(); 
-  StaticJsonDocument<128> doc;
+  StaticJsonDocument<200> doc;
   DeserializationError error = deserializeJson(doc, data);
   if (error) 
   {
@@ -62,30 +38,66 @@ void ConfigReveived(WebsocketsMessage message)
   }
   JsonObject TOUCHE1 = doc["action"];
   const char* TOUCHE1_action = doc["action"];
+  const char* TOUCHE1_isMedia = doc["isMedia"];
   const char* TOUCHE1_key1 = doc["key1"];
   const char* TOUCHE1_key2 = doc["key2"]; 
   const char* TOUCHE1_key3 = doc["key3"];
   Serial.println(TOUCHE1_action);
+  Serial.println(TOUCHE1_isMedia);
   Serial.println(TOUCHE1_key1);
   Serial.println(TOUCHE1_key2);
   Serial.println(TOUCHE1_key3);
-  //Convertir const char* en int
-  key1 = (char)strtol(TOUCHE1_key1, NULL, 0);
-  key2 = (char)strtol(TOUCHE1_key2, NULL, 0);
-  key3 = (char)strtol(TOUCHE1_key3, NULL, 0);
-  Serial.println(key1);
-  Serial.println(key2);
-  Serial.println(key3);
+  isMedia = TOUCHE1_isMedia;
+  Serial.println(isMedia);
+  if(isMedia == "false")
+  {
+    key1 = (char)strtol(TOUCHE1_key1, NULL, 0);
+    key2 = (char)strtol(TOUCHE1_key2, NULL, 0);
+    key3 = (char)strtol(TOUCHE1_key3, NULL, 0);
+  }
+  if(isMedia == "true")
+  {
+    char *copy = strdup(TOUCHE1_key1);
+    char *p = strtok(copy, ",");
+    int i = 0;
+    while (p != NULL) {
+    media[i++] = atoi(p);
+    p = strtok(NULL, ",");
+}
+free(copy);
+    //Serial.println(media[0]);
+    //Serial.println(media[1]);
+  }
+  // Serial.println(key1);
+  // Serial.println(key2);
+  // Serial.println(key3);
 }
 
 void KeyPressed(char key1, char key2, char key3)
 {
-  bleKeyboard.press(key1);
-  bleKeyboard.press(key2);
-  bleKeyboard.press(key3);
-  bleKeyboard.releaseAll();
-  delay(20);
-  while(digitalRead(button) == LOW);
+  int buttonPressTime;
+  if(isMedia == "false")
+  {
+    bleKeyboard.press(key1);
+    bleKeyboard.press(key2);
+    bleKeyboard.press(key3);
+    bleKeyboard.releaseAll();
+    delay(20);
+  }
+  if(isMedia == "true")
+  {
+    bleKeyboard.write(media);
+    buttonPressTime = millis();
+    while(digitalRead(button) == LOW){
+      if(millis() - buttonPressTime > 600)
+      {
+        bleKeyboard.press(media);
+        delay (20);
+      }
+    bleKeyboard.releaseAll();
+    }
+    
+  }
 }
 
 void Bluetooth(void * parameter)
@@ -96,6 +108,7 @@ void Bluetooth(void * parameter)
     {
       Serial.println("Button is pressed");
       KeyPressed(key1, key2, key3);
+      while(digitalRead(button) == LOW);
     }
     delay(20);
   }
@@ -143,7 +156,6 @@ void setup()
   // run callback when messages are received
   client.onMessage([&](WebsocketsMessage message)
   {
-    //Serial.println("Got Message : " + message.data());
     ConfigReveived(message);
   } );
 
